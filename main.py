be8e880f39905bc83289e192b5ee17fa41ea3a33
swell-d <3952800@gmail.com>
2022-06-27 21:15:30 +0200

    hello world bot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import telebot
 
@@ -8,3 +9,11 @@ bot = telebot.TeleBot(os.environ['SALARYINFOREIGNCURRENCY_BOT'])
 @bot.message_handler(commands=['start'])
 def start_command(message):
     bot.send_message(chat_id=message.chat.id, text='test')
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            bot.polling()
+        except:
+            time.sleep(60)

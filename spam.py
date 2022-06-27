4b0837551666b8a606683e4dc7defc25506e5e88
swell-d <3952800@gmail.com>
2022-06-28 00:04:19 +0200

    spam

---
+++ spam.py
@@ -0,0 +1,46 @@
+import json
+import os
+import time
+from datetime import datetime
+
+import redis
+import telebot
+from telebot import types
+
+from core import get_salary_text
+
+bot = telebot.TeleBot(os.environ['SALARYINFOREIGNCURRENCY_BOT'])
+db = redis.from_url(os.environ.get("REDIS_URL"))
+
+renew_menu = ['Проверить', 'Курс ЦБ', 'Графики',
+              'Справка', 'Сбросить']
+renew_menu_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+renew_menu_items = [types.KeyboardButton(each) for each in renew_menu]
+renew_menu_markup.row(*renew_menu_items[0:3])
+renew_menu_markup.row(*renew_menu_items[3:5])
+
+
+def send_salary(message_chat_id, val):
+    text = get_salary_text(val)
+    bot.send_message(chat_id=int(message_chat_id), text=text, reply_markup=renew_menu_markup)
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    sum, count, error = 0, 0, 0
+
+    for id in db.keys():
+        sum += 1
+        val = json.loads(db.get(id))
+        if val.get('state') != 255: continue
+        if (val.get('frequency') == 0) \
+                or (val.get('frequency') == 1 and datetime.today().isoweekday() == 1) \
+                or (val.get('frequency') == 2 and datetime.today().day == 1):
+            try:
+                send_salary(id, val)
+                count += 1
+            except:
+                error += 1
+
+    text = f"""Оповещено {count} из {sum} человек за {time.time() - start_time:.1f}c, {error} ошибок"""
+    bot.send_message(chat_id=211522613, text=text, reply_markup=renew_menu_markup)

7db57d65ed0900300422a6f20341fbae32909fd9
swell-d <3952800@gmail.com>
2023-05-29 12:16:49 +0200

    bugfix

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ bot = telebot.TeleBot(key)
 ALL_CURRENCIES = ['AUD', 'AZN', 'GBP', 'AMD', 'BYN', 'BGN', 'BRL', 'HUF', 'VND', 'HKD', 'GEL', 'DKK', 'AED', 'USD',
                   'EUR', 'EGP', 'INR', 'IDR', 'KZT', 'CAD', 'QAR', 'KGS', 'CNY', 'MDL', 'NZD', 'NOK', 'PLN', 'RON',
                   'XDR', 'SGD', 'TJS', 'THB', 'TRY', 'TMT', 'UZS', 'UAH', 'CZK', 'SEK', 'CHF', 'RSD', 'ZAR', 'KRW',
-                  'JPY']
+                  'JPY', 'RUB']
 CURRENCIES = ['RUB', 'UAH', 'BYN',
               'USD', 'EUR', 'GBP',
               'SEK', 'TRY', 'THB']
